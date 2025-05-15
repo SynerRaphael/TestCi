f9f30e7832f582a63ae4e9e2a4dcd877afc8033e
SynerRaphael <raphael.biard@synerglass-soft.com>
2025-05-15 15:23:31 +0200

    Update Web Service test auto deploy

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	// Route simple pour Hello World
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(200, "Holàààààà")
 	})
 
 	if err := router.SetTrustedProxies([]string{"127.0.0.1"}); err != nil {
